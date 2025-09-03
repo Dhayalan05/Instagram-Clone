39acffd0e07971c59740aef22ec5f2633dd96305
Dhayalan05 <thiyagupunitha21@gmail.com>
2025-09-03 16:52:48 +0530

    first commit

---
+++ src/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+function Sidebar() {
+  return (
+    <div className='m-3 position-fixed' >
+      <div className='d-flex flex-column gap-4'>
+        <img className='logo' src="src\assets\2.png" alt="" />
+        <div><i className="bi bi-house-door-fill"></i>Home</div>
+        <div><i className="bi bi-search"></i>Search</div>
+        <div><i className="bi bi-compass"></i>Explore</div>
+        <div><i className="bi bi-play-btn"></i>Reels</div>
+        <div><i className="bi bi-chat-dots"></i>Message</div>
+        <div><i className="bi bi-heart"></i>Notification</div>
+        <div><i className="bi bi-plus-square"></i>Create</div>
+        <div><i className="bi bi-person-circle"></i>Profile</div>
+      </div>
+      <div className='d-flex flex-column gap-4 position-fixed bottom-0 mb-3'>
+        <div><i className="bi bi-threads"></i>Threads</div>
+        <div><i className="bi bi-list"></i>More</div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Sidebar
